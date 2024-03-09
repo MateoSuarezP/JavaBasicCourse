af446fada9d1feb922405f9a465af39f3576dd62
Mateo <mateo_suarez@hotmail.com>
2024-03-09 15:01:03 -0500

    añado descripción de funciones

--- src/Funciones.java
+++ src/Funciones.java
@@ -13,15 +13,33 @@ public class Funciones {
 
         System.out.println("Peso a dolares: " +converToDolar(390000, "COP"));
     }
+    /**
+     * Descripción: Función que permite hallar el area de un circulo especificando su radio.
+     * @param r Radio del circulo
+     * */
     public static double circleArea (double r){
         return Math.PI * Math.pow(r, 2);
     }
+     /** Descripción: Función que permite hallar el de una esfera especificando su radio.
+     * @param r Radio de la esfera
+     * */
     public static double sphereArea (double r){
         return 4 * Math.PI * Math.pow(r, 2);
     }
+    /** Descripción: Función que permite hallar el volúmen de una esfera especificando su radio.
+     * @param r Radio de la esfera
+     * */
     public static double sphereVolume (double r){
         return (4/3) * Math.PI * Math.pow(r, 3);
     }
+
+    /**
+     * Descripción: Función que, especificando su moneda,convierte una cantidad de dinero en dolares
+     *
+     * @param quantity Cantidad dinero
+     * @param currency Tipo de moeda (solo acepta MXN y COP
+     * @return quantity devuelve la cantidad actualizada en dolares
+     * */
     public static double converToDolar (double quantity, String currency) {
         //MXN COP
         switch (currency){
